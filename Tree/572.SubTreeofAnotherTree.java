9cd7e9e2bfff4999645ff96007bd4d4be34fbaa2
Sumit Gupta <officialsumitgupta418@gmail.com>
2026-08-09 23:29:34 +0530

    Add subtree checking solution in 572.SubTreeofAnotherTree
    Implement a solution to check if one binary tree is a subtree of another.

---
+++ Tree/572.SubTreeofAnotherTree.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root==null){
+            return false;
+        }
+        if(func(root,subRoot)){
+            return true;
+        }
+        boolean left = isSubtree(root.left,subRoot);
+        boolean right = isSubtree(root.right,subRoot);
+        return left || right;
+    }
+  
+     public boolean func(TreeNode p,TreeNode q){
+        if(p==null && q==null){
+            return true;
+        }
+        if(p==null || q==null){
+            return false;
+        }
+        if(p.val != q.val){
+            return false;
+        }
+        boolean s1 = func(p.left,q.left);
+        boolean s2 = func(p.right, q.right);
+
+        return s1 && s2;
+    }
+}
